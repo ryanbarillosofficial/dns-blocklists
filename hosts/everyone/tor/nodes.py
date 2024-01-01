93468790bc3e59b511095371e3d3aa1d8975ce49
ryanbarillos <ryan.barillos@proton.me>
2024-01-01 15:29:32 -0700

    Attempt to fix

--- hosts/everyone/tor/nodes.py
+++ hosts/everyone/tor/nodes.py
@@ -51,7 +51,7 @@ def main():
     # Move IP addresses to respective list
     for node in nodes:
         if ipv4Regex.match(node): ipv4.append("0.0.0.0 " + node)  
-        elif ipv6Regex.match(node): ipv6.append("::0 " + node)  
+        elif ipv6Regex.match(node): ipv6.append("::1 " + node)  
     
     """
     Push results to .txt file:
